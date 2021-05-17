ab3ac2f281748c06f0b3ba151229b9e4b15bae43
IgnatInyutsin <iggyinyutsin@gmail.com>
2021-05-17 22:16:01 +0400

    migration ++

--- backend/test-requests/index.py
+++ backend/test-requests/index.py
@@ -1,4 +1,4 @@
 import requests
 
-res = requests.get('http://127.0.0.1:82/', data={'command': 'migration'})
+res = requests.get('http://tracker.perpetum-mobile.ru:82/', data={'command': 'migration'})
 print(res.text)
